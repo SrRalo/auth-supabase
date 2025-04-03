8dcff84c0e34da891456bba248435aa5c7cb335a
SrRalo <adreloa@gmail.com>
2025-04-03 14:07:42 -0500

    supabase

--- src/Supabase/SupabaseClient.js
+++ src/Supabase/SupabaseClient.js
@@ -3,6 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://ztzjacjoxwhyliswtyhx.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0emphY2pveHdoeWxpc3d0eWh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIzMzI4OTQsImV4cCI6MjA1NzkwODg5NH0.srLjrnMob8iMAzyAnzEFa7MwbYvA9XfURTTsZx7bJTM'
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabase = createClient(supabaseUrl, supabaseKey, {
+    auth: {
+      // URL a la que se redirigirá después de iniciar sesión
+      redirectTo: window.location.origin,
+    },
+  });
 
 export default supabase;
