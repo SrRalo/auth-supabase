289ab6b94614b72a6c1db589f65de45e42d68096
SrRalo <adreloa@gmail.com>
2025-04-03 13:59:07 -0500

    new-auths

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import './App.css'
+import Register from './components/Auth/Register'
 import Dashboard from './components/Dashboard'
-import Login from './components/Login'
+import Login from './components/Auth/Login'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Register />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   )
