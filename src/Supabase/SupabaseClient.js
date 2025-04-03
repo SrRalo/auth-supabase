adfa49c843547a7da9607a244d99c960adfa5a83
SrRalo <adreloa@gmail.com>
2025-04-03 14:08:50 -0500

    github-auth

--- src/Supabase/SupabaseClient.js
+++ src/Supabase/SupabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://ztzjacjoxwhyliswtyhx.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0emphY2pveHdoeWxpc3d0eWh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIzMzI4OTQsImV4cCI6MjA1NzkwODg5NH0.srLjrnMob8iMAzyAnzEFa7MwbYvA9XfURTTsZx7bJTM'
 
-const supabase = createClient(supabaseUrl, supabaseKey, {
+const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
       // URL a la que se redirigirá después de iniciar sesión
       redirectTo: window.location.origin,
