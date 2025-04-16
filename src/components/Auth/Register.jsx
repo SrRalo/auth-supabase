1ed5d025d0d6f60d49c0ca9444bd8c6f3277825d
SrRalo <adreloa@gmail.com>
2025-04-15 19:29:29 -0500

    sharkmin

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,9 +1,13 @@
 import { RegisterForm } from '../forms/register-form';
+import tubarao from '../../styles/tiburao.svg';
 
 function RegisterPage() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
+        <div className="mb-6">
+          <img src={tubarao} alt="Shark Logo" className="w-20 h-20 mx-auto dark:invert" />
+        </div>
         <RegisterForm />
       </div>
     </div>
